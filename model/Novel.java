cd43a68c3c9ff7c8726478c6e75f0e079cd0c68b
Subha12125 <subhabratapaul12125@gmail.com>
2026-01-20 12:29:45 +0530

    add Novel and text Book

---
+++ model/Novel.java
@@ -0,0 +1,23 @@
+package lms.model;
+
+public class Novel extends Book{
+    private String genre;
+    public Novel(int isbn, String name, String genre,boolean isAvailable){
+        super(isbn, name, isAvailable);
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public void displayBookDetails() {
+        System.out.println("Novel Details :");
+        System.out.println("The book title is: " + getName() + " and the genre is " + this.genre);
+    }
+}
